b98d20a6c3778c2802d7656900a3055ce5cfa034
Anthony Perez <none>
2017-12-24 15:53:17 -0800

    Added some skeleton code for parties

---
+++ party.attack.js
@@ -0,0 +1,15 @@
+// TODO, totally not done, also need to set up room data strctures
+
+/**
+ *
+ * First send out a scout, if the scout finds a suitable room then,
+ * From its home room the party should request a drop miner and a transporter
+ * The transporter's body should be calculated so that it exactly matches the miner's output
+ * Then have each do the appropriate thing
+ * The transporter should carry back energy to the home room, drop it, then create a request to pick it up.
+ *
+ **/
+
+module.exports = {
+
+};
---
+++ party.utilities.js
@@ -0,0 +1,13 @@
+// TODO, totally not done, also need to set up room data strctures
+
+// Party module should have the same methods as creep modules except without the 'suggested_body' method.  -- Also include an extra method to detect if the party is disbanded.
+// When a party module is created, it should check if Memory.parties exists and create it if it does not.
+// Parties should request resources, including spawning, from their home rooms.
+// Most parties should start with a scout to know what is needed.
+
+// This file should run all parties, regarless of rooms, and should run shutdown methods when parties are disbanded. -- Just like creep.utilities
+// Should also include a method for creating parties.
+
+module.exports = {
+
+};
